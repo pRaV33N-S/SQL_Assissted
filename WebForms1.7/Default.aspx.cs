1bf8b5b35d92358309503106e3b302b7734d9a5f
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-08-25 16:54:48 +0530

    WebForms 1.7

---
+++ WebForms1.7/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms1._7
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://maps.google.com");
+        }
+
+        protected void FileUpload1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void upload()
+        {
+
+        }
+
+    }
+}
