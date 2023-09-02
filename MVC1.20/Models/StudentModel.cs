240802217ce1b5f29bff3a36f77e49f2e0105a95
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-09-02 10:36:33 +0530

    MVC 1.20

---
+++ MVC1.20/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC1._20.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string WhichClass { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+
+
+    }
+}
