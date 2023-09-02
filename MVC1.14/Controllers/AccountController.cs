3f8e5648446a29abc88112769e37df448a741391
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-09-02 09:48:27 +0530

    MVC 1.14

---
+++ MVC1.14/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using MVC1._14.Models;
+using System.Security.Claims;
+
+
+namespace MVC1._14.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
