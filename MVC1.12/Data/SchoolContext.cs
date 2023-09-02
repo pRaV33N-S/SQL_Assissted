6cf4b5f4816d25375d53baf18dcbad45dc8840e8
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-09-02 09:35:39 +0530

    MVC 1.12

---
+++ MVC1.12/Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC1._12.Models;
+
+namespace MVC1._12.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC1._12.Models.Student> Student { get; set; } = default!;
+    }
+}
